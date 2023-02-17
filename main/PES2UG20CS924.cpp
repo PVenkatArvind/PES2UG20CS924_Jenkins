018c1f2bb6ab2016d2cab9e38a3d309a91d7c06f
P Venkat Arvind <79320301+PVenkatArvind@users.noreply.github.com>
2023-02-17 22:34:49 +0530

    task5

---
+++ main/PES2UG20CS924.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Creating Pipeline\n";
+  cout << "Name: P Venkat Arvind\n";
+  cout << "SRN: PES2UG20CS924\n";
+  return 0;
+}
